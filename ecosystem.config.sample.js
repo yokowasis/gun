0889d5f25a9a2ccc75138a29b57bfa250012343a
yokowasis <yokowasis@gmail.com>
2021-09-22 14:49:30 +0200

    eco

--- ecosystem.config.sample.js
+++ ecosystem.config.sample.js
@@ -1,12 +1,13 @@
 module.exports = {
   apps : {
     name   : "gun-server",
-    script : "./examples/http.js",
-    ignore_watch : ["node_modules", "public/images", "radata", "stats.radata", ".git/index.lock"],
+    script : "npm start",
+    watch : true,
+    ignore_watch : ["node_modules", "public/images", "radata", "stats.radata", ".git/index.lock", "*.log"],
     env : {
 	    "PORT" : "16902",
 	    "HTTPS_KEY" : "/home/admin/conf/web/server-excel.bimasoft.web.id/ssl/server-excel.bimasoft.web.id.key",
-	    "HTTPS_CERT" : "/home/admin/conf/web/server-excel.bimasoft.web.id/ssl/server-excel.bimasoft.web.id.crt",
+	    "HTTPS_CERT" : "/home/admin/conf/web/server-excel.bimasoft.web.id/ssl/combined-pem-crt.pem",
 	    "PEERS" : "https://gun-relay.bimasoft.web.id:16902/gun",
 	    "AXE" : false
     }
