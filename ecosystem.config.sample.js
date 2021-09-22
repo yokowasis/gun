d10268e00506be9c715028bb52c3ba69f846f7e9
yokowasis <yokowasis@gmail.com>
2021-09-22 07:43:07 +0200

    fix ecosystem config

--- ecosystem.config.sample.js
+++ ecosystem.config.sample.js
@@ -1,10 +1,8 @@
 module.exports = {
-  apps : [{
+  apps : {
     name   : "gun-server",
     script : "./examples/http.js",
-    watch : true,
-    ignore_watch : ["node_modules", "public/images", "radata", "stats.radata"],
-    instances : 1,
+    ignore_watch : ["node_modules", "public/images", "radata", "stats.radata", ".git/index.lock"],
     env : {
 	    "PORT" : "16902",
 	    "HTTPS_KEY" : "/home/admin/conf/web/server-excel.bimasoft.web.id/ssl/server-excel.bimasoft.web.id.key",
@@ -12,6 +10,6 @@ module.exports = {
 	    "PEERS" : "https://gun-relay.bimasoft.web.id:16902/gun",
 	    "AXE" : false
     }
-  }]
+  }
 }
 
